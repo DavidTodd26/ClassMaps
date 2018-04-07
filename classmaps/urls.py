e5022a733d0a9685656c83b3f6e04f8163f0a8b3
andsylin <andsylin@gmail.com>
2018-04-07 14:38:16 -0400

    Update urls.py

--- classmaps/urls.py
+++ classmaps/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('posts.urls')),
+    path('', include('classes.urls')),
     path('admin/', admin.site.urls),
 ]
