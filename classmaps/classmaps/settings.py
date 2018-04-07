d2c4fe3f56f4abe385314533e3a0b41bb12908d5
andsylin <andsylin@gmail.com>
2018-04-07 14:04:33 -0400

    Update settings.py

--- classmaps/classmaps/settings.py
+++ classmaps/classmaps/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
+    'classes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 import django_heroku
 django_heroku.settings(locals())
