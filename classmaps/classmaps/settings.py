8de002f9996356d8b908d0a61d03039d4a7bcf2b
andsylin <andsylin@gmail.com>
2018-04-07 13:57:17 -0400

    Update settings.py

--- classmaps/classmaps/settings.py
+++ classmaps/classmaps/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import django_heroku
+django_heroku.settings(locals())
