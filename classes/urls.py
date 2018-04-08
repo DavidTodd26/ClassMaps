d838abc2792425d7e01e51226cf49f78c1aa0b82
andsylin <andsylin@gmail.com>
2018-04-08 13:52:35 -0400

    Add files via upload

--- classes/urls.py
+++ classes/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, re_path
 from . import views
+from .views import search
 
+app_name = 'classes'
 urlpatterns = [
     path('', views.index, name="index"),
+    path('results/', search, name="search"),
+    re_path(r'details/(?P<id>\d+)/', views.details, name="details"),
 ];
--- classes/views.py
+++ classes/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Section
+from django.db.models import Q
 
 # Create your views here.
 
@@ -7,3 +9,21 @@ def index(request):
 
     }
     return render(request, 'classes/index.html', context)
+
+def details(request, id):
+    classe = Section.objects.get(id=id)
+    context = {
+        'classe': classe
+    }
+
+    return render(request, 'classes/details.html', context)
+
+def search(request):
+    template = 'classes/searches.html'
+    query = request.GET.get('q')
+    results = Section.objects.filter(Q(course__icontains=query)) | Section.objects.filter(Q(number__icontains=query)) 
+    context = {
+        'classes': results
+    }
+
+    return render(request, template, context)
