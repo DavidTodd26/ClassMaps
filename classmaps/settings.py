c94c46bef6b86aced89f7f2ebf275b1228962352
andsylin <andsylin@gmail.com>
2018-04-08 13:53:31 -0400

    Add files via upload

--- classmaps/settings.py
+++ classmaps/settings.py
@@ -120,7 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-import django_heroku
-django_heroku.settings(locals())
--- classmaps/urls.py
+++ classmaps/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('classes.urls')),
     path('admin/', admin.site.urls),
+    path('classes/', include('classes.urls')),
 ]
