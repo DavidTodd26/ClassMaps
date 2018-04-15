cef5311c0ec02f3fa5b54a495a70886915a0d23f
David Andrew Todd <datodd2634@gmail.com>
2018-04-15 14:45:06 -0400

    Fixed bad path name

--- classmaps/settings.py
+++ classmaps/settings.py
@@ -136,7 +136,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'classes/static/classes'),
 )
 
 import django_heroku
