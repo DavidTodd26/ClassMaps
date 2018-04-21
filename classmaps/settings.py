b9e2cf15a314a4b90daa1d1e6a493c8231e23403
Yang Song <yangsong@princeton.edu>
2018-04-21 12:52:38 -0400

    Changed settings to include CAS

--- classmaps/settings.py
+++ classmaps/settings.py
@@ -56,10 +56,13 @@ MIDDLEWARE = [
 MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django_cas.middleware.CASMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
 )
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
+    'django_cas_ng.backends.CASBackend',
     'django_cas.backends.CASBackend',
 )
 
@@ -127,7 +130,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+CAS_SERVER_URL = 'https://fed.princeton.edu/cas/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
