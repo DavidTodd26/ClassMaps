0e3efdd5448e16dc4fa6016e602d870eac24aa02
Yang Song <yangsong@princeton.edu>
2018-04-21 13:36:36 -0400

    changed something else again

--- classmaps/settings.py
+++ classmaps/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django_cas.middleware.CASMiddleware',
+    'django_cas_ng.middleware.CASMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
 )
@@ -130,6 +130,7 @@ USE_L10N = True
 USE_TZ = True
 
 CAS_SERVER_URL = 'https://fed.princeton.edu/cas/'
+CAS_VERSION = '3'
 
 CAS_PROXY_CALLBACK = url(r'^accounts/callback$', django_cas_ng.views.callback, name='cas_ng_proxy_callback')
 # Static files (CSS, JavaScript, Images)
