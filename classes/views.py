2d8f7655225101ef56f287bc2670c7efca0a7758
Adrian Tong <adriantongusa@gmail.com>
2018-04-29 10:56:11 -0400

    change display of search queries

--- classes/views.py
+++ classes/views.py
@@ -157,7 +157,7 @@ def getDayString(mon, tues, wed, thurs, fri):
     if (fri != None):
         days += " Friday,"
     if (days):
-        days = "," + days
+        days = "on" + days
     return days[:-1]
 
 @login_required
@@ -176,7 +176,7 @@ def search(request):
         results2 = searchTime(time, results2)
         if (not query and mon == None and tues == None and wed == None and thurs == None and fri == None):
             results2 = searchTime(time, Section.objects.all())
-        time = ", " + time
+        time = "at" + time
     if (time == None):
         time = ""
     dayString = getDayString(mon, tues, wed, thurs, fri)
