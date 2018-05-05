0339b950b6fa4be384c64a01d7d3f4485ef78d0d
Yang Song <yangsong@princeton.edu>
2018-05-05 13:15:45 -0400

    updated water locations

--- classes/static/classes/water.js
+++ classes/static/classes/water.js
@@ -1,10 +1,10 @@
 waterLocs = [[40.344889, -74.657054, 'Kitchen next to seminar room'], //1915
- //['40.342738', '-74.657116', 'Ground floor near bathrooms; first floor near room 211; second floor near room 221; third floor near room 231 (4)'], //1927
- //['40.348357', '-74.648976', 'Laundry Room'], //1939
- //['40.349043', '-74.6586', 'First (107), second (207), third (307) and fourth (407) floor kitchens; add-on wall units across rooms B204 and B304 (5)'], //1967
- //['40.350182', '-74.652298', 'Add-on wall units across from rooms D114, D214 and D314; in first, second and third floor kitchens (3)'], //1976
- [40.343557, -74.65567, 'Add on wall units in basement next to WPRB radio station and first floor lounge; sinks in study rooms 253, 353, 215 and 315; sinks in kitchen on first floor (6)']]; //bloomberg
- //['40.350484', '-74.651539', 'First floor kitchen sink'], //dodge osborne
- //['40.349582', '-74.657595', 'First (109A), second (205 and 207) and third floor (307) kitchens (3)'], //wilf
- //['40.342027', '-74.661065', 'Second Floor, one next to Butler College Office, one next to Wilson College Office (2)'], //wu-wilcox
- //['40.344018', '-74.65084', 'First and second Floor kitchens (2)']]; //yoseloff
+ [40.345247, -74.655653, 'Ground floor near bathrooms; first floor near room 211; second floor near room 221; third floor near room 231 (4)'], //1927
+ [40.345231, -74.656383, 'Laundry Room'], //1939
+ [40.34392, -74.655903, 'First (107), second (207), third (307) and fourth (407) floor kitchens; add-on wall units across rooms B204 and B304 (5)'], //1967
+ [40.344394, -74.655624, 'Add-on wall units across from rooms D114, D214 and D314; in first, second and third floor kitchens (3)'], //1976
+ [40.343557, -74.65567, 'Add on wall units in basement next to WPRB radio station and first floor lounge; sinks in study rooms 253, 353, 215 and 315; sinks in kitchen on first floor (6)'] //bloomberg
+ [40.345498, -74.655801, 'First floor kitchen sink'], //dodge osborne
+ [40.344349, -74.655071, 'First (109A), second (205 and 207) and third floor (307) kitchens (3)'], //wilf
+ [40.344734, -74.656289, 'Second Floor, one next to Butler College Office, one next to Wilson College Office (2)'], //wu-wilcox
+ [40.344186, -74.655796, 'First and second Floor kitchens (2)']]; //yoseloff
