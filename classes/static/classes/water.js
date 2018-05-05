f9d26d96177ee5d7618827e2282f60591e913e5a
Yang Song <yangsong@princeton.edu>
2018-05-05 13:17:14 -0400

    added missing comma

--- classes/static/classes/water.js
+++ classes/static/classes/water.js
@@ -3,7 +3,7 @@ waterLocs = [[40.344889, -74.657054, 'Kitchen next to seminar room'], //1915
  [40.345231, -74.656383, 'Laundry Room'], //1939
  [40.34392, -74.655903, 'First (107), second (207), third (307) and fourth (407) floor kitchens; add-on wall units across rooms B204 and B304 (5)'], //1967
  [40.344394, -74.655624, 'Add-on wall units across from rooms D114, D214 and D314; in first, second and third floor kitchens (3)'], //1976
- [40.343557, -74.65567, 'Add on wall units in basement next to WPRB radio station and first floor lounge; sinks in study rooms 253, 353, 215 and 315; sinks in kitchen on first floor (6)'] //bloomberg
+ [40.343557, -74.65567, 'Add on wall units in basement next to WPRB radio station and first floor lounge; sinks in study rooms 253, 353, 215 and 315; sinks in kitchen on first floor (6)'], //bloomberg
  [40.345498, -74.655801, 'First floor kitchen sink'], //dodge osborne
  [40.344349, -74.655071, 'First (109A), second (205 and 207) and third floor (307) kitchens (3)'], //wilf
  [40.344734, -74.656289, 'Second Floor, one next to Butler College Office, one next to Wilson College Office (2)'], //wu-wilcox
