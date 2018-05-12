decf2cdff8391f1e02746219a7790f730080b12d
David Andrew Todd <datodd2634@gmail.com>
2018-05-12 19:18:39 -0400

    Try another fix

--- classmaps/settings.py
+++ classmaps/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2w*$)(mt^!1vn_i1k050$mi#1s)ge%l87b(u1i$yq5*(dfum29'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://classmaps.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
