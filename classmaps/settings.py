4b40d01b1121edba74db716e4cfcb2a3fe7eaca7
David Andrew Todd <datodd2634@gmail.com>
2018-05-12 19:58:02 -0400

    Test

--- classmaps/settings.py
+++ classmaps/settings.py
@@ -138,6 +138,7 @@ CAS_VERSION = '3'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
