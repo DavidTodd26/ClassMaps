73c86207589d1d1de1d19bf06fb32fe21c53fa0f
David Andrew Todd <datodd2634@gmail.com>
2018-05-12 19:49:04 -0400

    Try again

--- classmaps/settings.py
+++ classmaps/settings.py
@@ -138,6 +138,11 @@ CAS_VERSION = '3'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-import django_heroku
-django_heroku.settings(locals())
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+#import django_heroku
+#django_heroku.settings(locals())
