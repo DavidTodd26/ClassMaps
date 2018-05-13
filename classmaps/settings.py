b11c44c9fd5391d095a9c6274b852e9e54c66468
David Andrew Todd <datodd2634@gmail.com>
2018-05-12 21:26:24 -0400

    Revert

--- classmaps/settings.py
+++ classmaps/settings.py
@@ -138,7 +138,7 @@ CAS_VERSION = '3'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_DIRS = [
