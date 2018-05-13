b3f7a5d6a73d04f1424fef5d535339d1826ebdb5
David Andrew Todd <datodd2634@gmail.com>
2018-05-12 22:07:00 -0400

    Restrict allowed hosts

--- classmaps/settings.py
+++ classmaps/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['classmaps.herokuapp.com','*']
+ALLOWED_HOSTS = ['classmaps.herokuapp.com']
 
 
 # Application definition
