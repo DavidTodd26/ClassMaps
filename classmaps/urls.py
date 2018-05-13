4beed4994350745aeeece2792e913ef7431ffbdb
David Andrew Todd <datodd2634@gmail.com>
2018-05-12 20:18:18 -0400

    Restore

--- classmaps/urls.py
+++ classmaps/urls.py
@@ -14,9 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('classes.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
