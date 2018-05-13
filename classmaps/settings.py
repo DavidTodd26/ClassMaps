65cf19c0defd0eee5bb3647ff6547664fa72b9d1
David Andrew Todd <datodd2634@gmail.com>
2018-05-12 21:24:56 -0400

    Revert

--- classmaps/settings.py
+++ classmaps/settings.py
@@ -138,12 +138,12 @@ CAS_VERSION = '3'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'static'),
-]
+#STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+#]
 
 import django_heroku
 django_heroku.settings(locals())
